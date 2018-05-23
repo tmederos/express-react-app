2654c7d5fda4677cf52dad95af50261d18d28722
Trish Mederos <trishmederos@gmail.com>
2018-05-23 09:04:20 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));  
+}
+
+
+app.get('/api/test', (req,res) => {
+  res.json({
+    message: 'Hello Trish Express!',
+  })
+});
+
+app.listen(PORT, ()=>{
+  console.log('Listening on port '+ PORT);
+});
