5c3723f20bad367b4eb575c42c0e0715e79b7fd5
Trish Mederos <trishmederos@gmail.com>
2018-05-29 13:30:59 -0700

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -11,33 +11,66 @@ import MessageField from './components/MessageField'
 
 class App extends Component {
 
-  state = {
-    username: 'User',
-    messages: [],
-  };
+	state = {
+		username: 'User',
+		messages: [],
+	}
 
-  componentWillMount(){
-    getMessages().then(messages => {
-      this.setState({messages})
-    }).catch(err => {
-      console.error(err)
-    })
-  };
+	handleRename = username => {
+		this.setState({username})
+	}
+
+	handleSend = message => {
+		const messageModel = {
+			sender: this.state.username,
+			body: message,
+		}
+		sendMessage(messageModel).then(message => {
+			this.setState({messages: [...this.state.messages, message]})
+		}).catch(err => {
+			console.error(err)
+		})
+	}
+
+	pollMessages = () => {
+		setInterval(() => {
+			getMessages().then(messages => {
+				this.setState({messages})
+			}).catch(err => {
+				console.error(err)
+			})
+		}, 2000)
+	}
+
+	componentWillMount () {
+		getMessages().then(messages => {
+			this.setState({messages})
+		}).catch(err => {
+			console.error(err)
+		})
+		this.pollMessages()
+	}
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello {this.state.heading}</h1>
+          <h1 className="App-title">Hello {this.state.username}</h1>
         </header>
-        //<nameField username={this.state.username} handleRename={this.handleRename}/>
-        <ChatWindow>
-          {this.state.messages.map(message =>{
-            <ChatMessage sender={message.sender} body={message.body} date={message.date}/>
-          })}
-          //<MessageField message={this.state.message} handleSend={this.handleSend}/> 
-        </ChatWindow>
+
+		<NameField username={this.state.username} handleRename={this.handleRename}/>
+		<ChatWindow>
+			{this.state.messages.map(message => (
+				<ChatMessage key={message._id}
+          sender={message.sender}
+          body={message.body}
+          date={message.date}
+          username={this.state.username}
+        />
+			))}
+		</ChatWindow>
+		<MessageField message={this.state.message} handleSend={this.handleSend}/>
       </div>
     );
   }
--- client/src/components/ChatMessage/ChatMessage.js
+++ client/src/components/ChatMessage/ChatMessage.js
@@ -1,15 +1,21 @@
-import React from 'react'
 
-const ChatWindow = props => (
-  <p class = "chat-message">
-    <span class="chat-timestamp">
-      {props.date}
-    </span>
-    <span class = "chat-sender">
-      {this.props.sender}:
-    </span>
-    <span class = "chat-body">
-      {this.props.body}:
-    </span>
-  </p>
+import React from 'react'
+import moment from 'moment'
+
+import './ChatMessage.css'
+
+const ChatMessage = props => (
+	<p className="chat-message">
+		<span className="chat-timestamp">
+			{moment(props.date).format('h:mm:ss a')}
+		</span>
+		<span className={props.username === props.sender ? 'chat-sender-user' : 'chat-sender-else'}>
+			{props.sender}:
+		</span>
+		<span className="chat-body">
+			{props.body}
+		</span>
+	</p>
 )
+
+export default ChatMessage
